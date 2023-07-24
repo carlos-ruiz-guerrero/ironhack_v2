6282fc7ecefe9de6504513e534e02d1143e1faab
carlosruiz <122636522+carlosruiz-stack@users.noreply.github.com>
2023-07-24 20:22:37 +0200

    Update job_openings_db.'ds_salaries(1)'.queries.sql
    Update queries

--- job_openings_db.'ds_salaries(1)'.queries.sql
+++ job_openings_db.'ds_salaries(1)'.queries.sql
@@ -1,5 +1,3 @@
-use job_openings_db;
-
 SELECT * FROM job_openings_db.`ds_salaries (1)`;
 
 # Provide a top 20 of the entry-level positions with the highest pay in the year 2022 
@@ -9,13 +7,18 @@ WHERE experience_level = 'EN' AND work_year = 2022
 ORDER BY salary_in_usd DESC
 LIMIT 20;
 
-# Calculate the average salary of a machine learning engineer in spain for the yer 2022
+# Calculate the average salary of a machine learning engineer 
 
 SELECT AVG(salary) AS average_salary
 FROM job_openings_db.`ds_salaries (1)`
-WHERE job_title = 'Machine Learning Engineer'
-  AND salary_currency = 'EUR'
+WHERE job_title = 'Machine Learning Engineer' AND experience_level = 'MI';
 
+# Select the job openings in Spain that are not 'experienced level' (EX) and provide the first 20 positions, order by job title 
+
+SELECT salary, job_title, experience_level, company_size FROM job_openings_db.`ds_salaries (1)`
+WHERE company_location = 'ES' AND experience_level !='EX'
+ORDER BY job_title ASC
+LIMIT 20;
 
   
 
